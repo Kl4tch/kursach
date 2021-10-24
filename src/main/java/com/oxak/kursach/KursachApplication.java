3dfa66cf9624eeee7c53f544b0c429cb81e9ee90
timur <oksak208@gmail.com>
2021-10-24 12:14:35 +0300

    Добавлен логин

--- src/main/java/com/oxak/kursach/KursachApplication.java
+++ src/main/java/com/oxak/kursach/KursachApplication.java
@@ -1,9 +1,12 @@
 package com.oxak.kursach;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class KursachApplication {
 
 	public static void main(String[] args) {
